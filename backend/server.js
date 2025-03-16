a9630ed7377cbfd9e094561bef829f959cab95bd
Monish Kiran <monishkiran304@gmail.com>
2025-03-17 00:04:45 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from '../config/db.js';
+import Product from '../models/product.model.js';
+import mongoose from 'mongoose';
+import productRoutes from "./routes/product.route.js"
+
+dotenv.config();
 
 const app = express();
+// middleware - is a dunction that rusn before you send a response back to the client
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.get("/", (req,res) =>{
-    res.send("Server is ready");
-})
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
-    console.log("Server started at https://localhost:5000");
+    connectDB();
+    console.log("Server started at http://localhost:5000");
 });
+
+
